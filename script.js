029a79d63062782187a02cb15d72c997709e6c4b
ajcc11431 <67560236+ajcc11431@users.noreply.github.com>
2021-03-18 17:59:07 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// this is js (javascript)
+// console.log is only visible in the console tab in inspect element
+
+document.getElementById("btnt").onclick = function() {
+    location.href = "games.html";
+}
+
+document.getElementById("gitr").onclick = function() {
+    location.href = "https://github.com/ajcc11431/projectf1";
+}
